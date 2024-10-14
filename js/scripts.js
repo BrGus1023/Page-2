17fd9e5e0c88a5a13c47e441afff025090defdb9
BrGus1023 <brgus1023@gmail.com>
2024-10-13 19:03:17 -0600

    Commit 1

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+//Leaflet Map
+
+var map = L.map('map').setView([19.841666666667, -98.989166666667], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
